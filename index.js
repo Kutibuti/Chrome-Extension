69342aaecd13c058ce6e81fd8f8718910bdf6489
Ömer Faruk Ayaz <omerfayaz166@gmail.com>
2023-01-22 21:19:06 +0300

    Some changes

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBTn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) 
+deleteBTn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
 console.log(leadsFromLocalStorage)
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         //listItems += "<li> <a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a> </li>"
         listItems +=
             `
             <li>
-                <a target="_blank" href="${myLeads[i]}">
-                ${myLeads[i]}
+                <a target="_blank" href="${leads[i]}">
+                ${leads[i]}
                 </a> 
             </li>
             `
@@ -33,5 +44,4 @@ function renderLeads() {
         //ulEl.append(li)
     }
     ulEl.innerHTML = listItems
-    //inputEl.value = ""
 }

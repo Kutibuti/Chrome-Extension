b4949bdaaef4a5ffa7f6c4cd1acfdf53f2add5ae
Ömer Faruk Ayaz <omerfayaz166@gmail.com>
2023-01-23 15:23:34 +0300

    Extension completed

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBTn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+const tabs = [
+    { url: "https://www.linkedin.con/in/per-harald-borgen/" }
+]
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
@@ -14,7 +18,16 @@ inputBtn.addEventListener("click", function () {
     render(myLeads)
 })
 
-deleteBTn.addEventListener("dblclick",function(){
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log(tabs[0].url);
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBTn.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
     render(myLeads)
@@ -24,13 +37,11 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
-
 console.log(leadsFromLocalStorage)
 
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-        //listItems += "<li> <a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a> </li>"
         listItems +=
             `
             <li>
